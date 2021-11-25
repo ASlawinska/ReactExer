3636ce53aabbe0b94d649d0b84c7cda5f0d79fb2
ASlawinska <slawinska.adrianna@gmail.com>
2021-11-25 11:17:22 +0100

    Added methods

--- 10_koszyk/app.js
+++ 10_koszyk/app.js
@@ -1,13 +1,25 @@
 class App extends React.Component {
     state={
         avaliableProducts: 7,
+        shoppingCard: 0,
     }
+    handleRemoveFromCard = () => {
+        this.setState({
+            shoppingCard: this.state.shoppingCard -1
+        })
+    }
+    handleAddToCard = () => {
+        this.setState({
+            shoppingCard: this.state.shoppingCard +1
+        })
+    }
+
     render() {
         return(
             <>
-                <button disabled>-</button>
-                <span> 0 </span>
-                <button>+</button>
+                <button disabled={this.state.shoppingCard ? false : true} onClick={this.handleRemoveFromCard}>-</button>
+                <span>{this.state.shoppingCard}</span>
+                <button disabled={this.state.shoppingCard === this.state.avaliableProducts ? true : false} onClick={this.handleAddToCard}>+</button>
             </>
         )
     }
